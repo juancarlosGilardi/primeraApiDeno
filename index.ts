0ca921ffd387c6ccf071351975aa1f7e99d6a302
JuanCarlosPlus <63310685+JuanCarlosPlus@users.noreply.github.com>
2020-07-18 22:48:53 -0500

    first commit

---
+++ index.ts
@@ -0,0 +1,11 @@
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+
+import router from "./routes/index.routes.ts";
+
+const app = new Application();
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+console.log("Servidor corriendo en el puerto,3000)");
+await app.listen({ port: 3000 });
